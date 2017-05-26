5fb763eaf0b22035c24d7c82ffff558f2ea7e3a8
Kaito Kishi <kaito@kishi.com>
2017-05-26 09:38:50 +0900

    Run.

--- moyf/ml/nn.py
+++ moyf/ml/nn.py
@@ -11,39 +11,48 @@ class Model:
             self.biases = []
 
         def add(self, layer):
-            if len(self.layers) == 0 and layer.input_num is None:
-                raise Exception('There is not input_num!')
+            if layer.input_num is None:
+                if len(self.layers) == 0:
+                    raise Exception('There is not input_num!')
+                else:
+                    layer.input_num = self.layers[-1].output_num
 
-            layer.input_num = self.layers[-1].output_num
             self.layers.append(layer)
             self.weights.append(np.random.normal(0, 1, size=(layer.output_num, layer.input_num)))
-            self.biases.append(np.zeros(shape=(layer.output_num, layer.input_num)))
+            self.biases.append(np.zeros(shape=(layer.output_num, 1)))
 
     def __init__(self, optimizer=None, loss=None):
-        self.optimizer = optimizer
+        #self.optimizer = optimizer TODO
         self.loss = loss
-        self.layers = Layers()
+        self.layers = Model.Layers()
 
     def learn(self, input_train, output_train, epoch_num, learning_rate=0.01):
         for epoch in range(epoch_num):
             last_outputs = []
-            not_activated_outputs = []
-            outputs = []
+            not_activated_outputs = [[] for i in self.layers.layers]
+            outputs = [[] for i in self.layers.layers]
             
             for input_train_unit in input_train:
                 not_activated_outputs_unit = []
                 outputs_unit = []
 
-                previous_output = input_train_unit
-                for layer, weight, bias in zip(self.layers.layers, self.layers.weights, self.layers.biases):
-                    not_activated_output = weight.dot(previous_output) + bias
-                    not_activated_outputs_unit.append(not_activated_output)
+                previous_output = np.array([input_train_unit])
+                for layer, weight, bias, i in zip(self.layers.layers, self.layers.weights, self.layers.biases, range(len(self.layers.layers))):
+#                    import ipdb; ipdb.set_trace()
+                    not_activated_output = weight.dot(previous_output.reshape(len(previous_output), 1)) + bias
 
-                    previous_output = layer.activate(not_activated_output)
-                    outputs_unit.append(previous_output)
+                    previous_output = layer.activate(not_activated_output.transpose()[0]).transpose()
+#                    import ipdb; ipdb.set_trace()
 
-                not_activated_outputs.append(np.array(not_activated_outputs_unit))
-                outputs.append(np.array(outputs_unit))
+                    if len(not_activated_outputs[i]) == 0:
+                        not_activated_outputs[i].append(not_activated_output)
+                        outputs[i].append(previous_output.reshape(len(previous_output), 1))
+                        #outputs_unit.append(previous_outputs)
+                    else:
+                        not_activated_outputs[i] = [np.hstack((not_activated_outputs[i][0], not_activated_output))]
+                        outputs[i] = [np.hstack((outputs[i][0], previous_output.reshape(len(previous_output), 1)))]
+#                not_activated_outputs.append(np.array(not_activated_outputs_unit))
+#                outputs.append(np.array(outputs_unit))
 
                 last_outputs.append(previous_output)
 
@@ -53,33 +62,44 @@ class Model:
             dif_biases = []
 
             # delta L(length - 1)
-            deltas.append(output_train - last_outputs)
-
             length = len(self.layers.layers)
-            for i in range(length):
-                deltas.insert(0, common.dif(self.loss, not_activated_outputs[length - i - 2]) * (self.layers.weights[length - i - 1].dot(deltas[0])))
+            deltas.append((output_train - np.array(last_outputs).transpose()))
+            dif_weights.insert(0, (1 / len(input_train)) * deltas[0].dot(outputs[length - 2][0].transpose()))
+            dif_biases.insert(0, (1 / len(input_train)) * deltas[0].dot(np.ones(shape=(len(input_train), 1))))
 
-                dif_weights.insert(0, (1 / len(input_train)) * deltas[0].dot(outputs[length - i - 3].transpose()))
+            for i in range(length):
+                for k in range(len(not_activated_outputs[length - i - 2][0])):
+                    not_activated_outputs[length - i - 2][0][k] = common.dif(self.layers.layers[length - i - 2].activate, not_activated_outputs[length - i - 2][0][k])
+                    #not_activated_outputs[j] = common.dif(self.loss, not_activated_outputs[length - i - 2][j])
+                #import ipdb; ipdb.set_trace()
+                deltas.insert(0, not_activated_outputs[length - i - 2][0] * (self.layers.weights[length - i - 1].transpose().dot(deltas[0])))
+
+                dif_weights.insert(0, (1 / len(input_train)) * deltas[0].dot(outputs[length - i - 3][0].transpose()))
                 dif_biases.insert(0, (1 / len(input_train)) * deltas[0].dot(np.ones(shape=(len(input_train), 1))))
 
+                if length - i - 2 == 0:
+                    break
+
             for i in range(length):
-                weights[i] -= learning_rate * dif_weights[i]
-                biases[i] -= learning_rate * dif_biases[i]
+#                import ipdb; ipdb.set_trace()
+                self.layers.weights[i] -= learning_rate * dif_weights[i]
+                self.layers.biases[i] -= learning_rate * dif_biases[i]
 
     def result(self, input_result):
         last_outputs = []
 
         for input_result_unit in input_result:
-            previous_output = input_result
+            previous_output = np.array([input_result_unit])
             for layer, weight, bias in zip(self.layers.layers, self.layers.weights, self.layers.biases):
-                not_activated_output = weight.dot(previous_output) + bias
-                previous_output = layer.activate(not_activated_output)
+#                import ipdb; ipdb.set_trace()
+                not_activated_output = weight.dot(previous_output.reshape(len(previous_output), 1)) + bias
+                previous_output = layer.activate(not_activated_output.transpose()[0]).transpose()
             last_outputs.append(previous_output)
 
         return np.array(last_outputs)
 
 class Layer:
-    def __init__(self, input_num=None, output_num, activate):
+    def __init__(self, activate, output_num, input_num=None):
         self.input_num = input_num
         self.output_num = output_num
         self.activate = activate
--- sin.py
+++ sin.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 from moyf.ml import nn, common
 
 def main():
-    x_train = np.arange(-3, 3, 0.5)
+    x_train = np.arange(-3, 3, 0.2)
     y_train = np.sin(x_train)
     y_train = [it + random.gauss(0, 0.2) for it in y_train]
 
-    model = nn.Model(optimizer=nn.Sgd, loss=common.mse)
+    model = nn.Model(loss=common.mse)
 
     model.layers.add(nn.Layer(input_num=1, output_num=5, activate=common.relu))
     model.layers.add(nn.Layer(output_num=4, activate=common.relu))
     model.layers.add(nn.Layer(output_num=1, activate=common.linear))
 
-    model.learn(input_train=x_train, output_train=y_train, epoch_num=5, batch_num=6)
+    model.learn(input_train=x_train, output_train=y_train, epoch_num=5)
 
     x_result = np.arange(-3, 3, 0.1)
     y_result = model.result(input_result=x_result)
